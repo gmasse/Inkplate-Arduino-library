8bb93d682397d8e414ef5fdbb2f3a294728dcaec
Štefan Granatir <116319250+stefangranatir@users.noreply.github.com>
2025-08-12 14:54:54 +0200

    Feedback fix

--- examples/Inkplate5V2/Basic/Inkplate5V2_Partial_Update/Inkplate5V2_Partial_Update.ino
+++ examples/Inkplate5V2/Basic/Inkplate5V2_Partial_Update/Inkplate5V2_Partial_Update.ino
@@ -29,6 +29,7 @@ const char text[] = "This is partial update on Inkplate 5V2 e-paper display! :)"
 // This variable is used for moving the text (scrolling)
 int offset = E_INK_WIDTH;
 
+//This variable is used to define the number of partial updates before doing a full update
 int partialUpdates=9;
 
 void setup()
@@ -39,7 +40,13 @@ void setup()
     display.setTextColor(BLACK, WHITE); // Set text color to be black and background color to be white
     display.setTextSize(4);             // Set text to be 4 times bigger than classic 5x7 px text
     display.setTextWrap(false);         // Disable text wraping
-    display.setFullUpdateThreshold(partialUpdates); //Set the number of partial updates before doing a full update
+    /*
+    Set the number of partial updates before doing a full update
+    This function forces a full update as the next update to ensure that the cycle of partial 
+    updates starts from a fully updated screen.
+    The Inkplate class keeps a internal counter that increments every time partialUpdate() gets called.
+    */
+    display.setFullUpdateThreshold(partialUpdates);
 }
 
 void loop()
@@ -49,21 +56,17 @@ void loop()
     display.clearDisplay();         // Clear content in frame buffer
     display.setCursor(offset, 300); // Set new position for text
     display.print(text);            // Write text at new position
-    display.partialUpdate(false, true); // Do partial update
+
+    /*
+    //Updates changes parts of the screen without the need to refresh the whole display
+    //partialUpdate(bool _forced, bool leaveOn)
+	    _forced		Can force partial update in deep sleep (for advanced use)
+	    leaveOn 	If set to 1, it will disable turning power supply for eink after display update in order to increase refresh time
+    */
+    display.partialUpdate(false, true);
     offset -= 20; // Move text into new position
     if (offset < 0)
         offset = 800; // Text is scrolled till the end of the screen? Get it back on the start!
     delay(500);       // Delay between refreshes.
 
-    // ADVANCED USAGE
-
-    display.clearDisplay();         // Clear content in frame buffer
-    display.setCursor(offset, 300); // Set new position for text
-    display.print(text);            // Write text at new position
-    display.einkOn(); // Turn on e-ink display
-    display.partialUpdate(false, true); // Do partial update
-    offset -= 20; // Move text into new position
-    if (offset < 0)
-        offset = 800; // Text is scrolled till the end of the screen? Get it back on the start!
-    delay(500);       // Delay between refreshes.
 }
