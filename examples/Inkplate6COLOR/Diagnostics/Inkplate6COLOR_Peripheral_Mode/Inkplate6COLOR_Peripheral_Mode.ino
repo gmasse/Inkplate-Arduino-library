ad280bead0ee1e13aea363529cacf33242eeb58e
mtoldi <marko.toldi58@gmail.com>
2025-10-23 12:17:33 +0200

    IP 6COLOR peripheral mode update

--- examples/Inkplate6COLOR/Diagnostics/Inkplate6COLOR_Peripheral_Mode/Inkplate6COLOR_Peripheral_Mode.ino
+++ examples/Inkplate6COLOR/Diagnostics/Inkplate6COLOR_Peripheral_Mode/Inkplate6COLOR_Peripheral_Mode.ino
@@ -1,298 +1,69 @@
 /*
-   Inkplate6COLOR_Peripheral_Mode example for Soldered Inkplate 6COLOR
+   Inkplate6COLOR_Peripheral_Mode sketch for Soldered Inkplate 6COLOR.
+   For this example you will need only a USB-C cable and Inkplate 6COLOR.
    Select "Soldered Inkplate 6COLOR" from Tools -> Board menu.
-   Don't have "Soldered Inkplate 6COLOR" option? Follow our tutorial and add it:
-   https://soldered.com/learn/add-inkplate-6-board-definition-to-arduino-ide/
+   Don’t have "Soldered Inkplate 6COLOR" option? Follow our tutorial and add it:
+   https://soldered.com/learn/add-inkplate-6color-board-definition-to-arduino-ide/
 
-   Using this sketch, you don't have to program and control e-paper using Arduino code.
-   Instead, you can send UART command. This give you flexibility that you can use this Inkplate 6COLOR on any platform!
+   Using this sketch, you don’t have to program and control the e-paper display using Arduino code.
+   Instead, you can send UART commands. This gives you the flexibility to use Inkplate 6COLOR on any platform!
 
-   Because it uses UART, it's little bit slower and it's not recommended to send bunch of
-   drawPixel command to draw some image. Instead, load bitmaps and pictures on SD card and load image from SD.
-   If we missed some function, you can modify this and make yor own.
-   Also, every Inkplate comes with this peripheral mode right from the factory.
+   Because it uses UART, it’s a bit slower and not recommended to send a large number of
+   drawPixel commands to render images. Instead, store bitmaps or pictures on an SD card
+   and load them directly from there.
+   If any functionality is missing, you can modify this code and make your own version.
+   Every Inkplate 6COLOR comes with this Peripheral Mode preloaded from the factory.
 
    Learn more about Peripheral Mode:
    https://inkplate.readthedocs.io/en/latest/peripheral-mode.html
 
-   UART settings are: 115200 baud, standard parity, ending with "\n\r" (both)
-   You can send commands via USB port or by directly connecting to ESP32 TX and RX pins.
-   Don't forget you need to send #L(1)* after each command to show it on the display
-   (equal to display.display()).
+   UART settings are: 115200 baud, standard parity, ending with "\n\r" (Both NL & CR)
+   You can send commands via the USB port or by directly connecting to the ESP32 TX and RX pins.
 
-   Want to learn more about Inkplate? Visit www.inkplate.io
-   Looking to get support? Write on our forums: https://forum.soldered.com/
-   15 July 2020 by Soldered
+   Want to learn more about Inkplate? Visit:
+   https://soldered.com/documentation/inkplate/
+
+   23 October 2025 by Soldered
 */
 
-// Next 3 lines are a precaution, you can ignore those, and the example would also work without them
-#ifndef ARDUINO_INKPLATECOLOR
-#error "Wrong board selection for this example, please select Soldered Inkplate 6COLOR in the boards menu."
-#endif
+// Include Inkplate library
+#include "Inkplate.h"
 
-#include <Inkplate.h>
+// Include Peripheral Mode library
+#include "InkplatePeripheralMode.h"
+
+// Include the header file with sketch settings (buffer size, serial timeout, argument termination char, etc.)
+#include "settings.h"
+
+// Pointer to the singleton Peripheral Mode instance
+PeripheralMode *peripheral;
+
+// Create Inkplate 6COLOR display object
 Inkplate display;
-
-#define BUFFER_SIZE 1000
-char commandBuffer[BUFFER_SIZE + 1];
-char strTemp[2001];
 
 void setup()
 {
+    // Initialize Inkplate library
     display.begin();
-    Serial.begin(115200);
-    memset(commandBuffer, 0, BUFFER_SIZE);
+
+    // Create instance of Peripheral Mode object
+    peripheral = PeripheralMode::getInstance();
+
+    // Initialize Peripheral Mode (UART, display, buffer, etc.)
+    if (!peripheral->begin(&Serial, &display, 115200ULL, SERIAL_UART_RX_PIN, SERIAL_UART_TX_PIN, SERIAL_BUFFER_SIZE))
+    {
+        // Send an error message to serial
+        Serial.println("Peripheral Mode init failed!\nProgram halted!");
+
+        // Stop program execution
+        while (1);
+    }
+
+    Serial.println("READY");
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
-    if (Serial.available())
-    {
-        while (Serial.available())
-        {
-            for (int i = 0; i < (BUFFER_SIZE - 1); i++)
-            {
-                commandBuffer[i] = commandBuffer[i + 1];
-            }
-            commandBuffer[BUFFER_SIZE - 1] = Serial.read();
-        }
-    }
-    char *s = NULL;
-    char *e = NULL;
-    for (int i = 0; i < BUFFER_SIZE; i++)
-    {
-        if (commandBuffer[i] == '#' && s == NULL)
-            s = &commandBuffer[i];
-        if (commandBuffer[i] == '*' && e == NULL)
-            e = &commandBuffer[i];
-    }
-    if (s != NULL && e != NULL)
-    {
-        if ((e - s) > 0)
-        {
-            int x, x1, x2, y, y1, y2, x3, y3, l, c, w, h, r, n, rx, ry, xc, yc;
-            char b;
-            char temp[150];
-            switch (*(s + 1))
-            {
-            case '?':
-                Serial.print("OK");
-                break;
-
-            case '0':
-                sscanf(s + 3, "%d,%d,%d", &x, &y, &c);
-                display.drawPixel(x, y, c);
-                break;
-
-            case '1':
-                sscanf(s + 3, "%d,%d,%d,%d,%d", &x1, &y1, &x2, &y2, &c);
-                display.drawLine(x1, y1, x2, y2, c);
-                break;
-
-            case '2':
-                sscanf(s + 3, "%d,%d,%d,%d", &x, &y, &l, &c);
-                display.drawFastVLine(x, y, l, c);
-                break;
-
-            case '3':
-                sscanf(s + 3, "%d,%d,%d,%d", &x, &y, &l, &c);
-                display.drawFastHLine(x, y, l, c);
-                break;
-
-            case '4':
-                sscanf(s + 3, "%d,%d,%d,%d,%d", &x, &y, &w, &h, &c);
-                display.drawRect(x, y, w, h, c);
-                break;
-
-            case '5':
-                sscanf(s + 3, "%d,%d,%d,%d", &x, &y, &r, &c);
-                display.drawCircle(x, y, r, c);
-                break;
-
-            case '6':
-                sscanf(s + 3, "%d,%d,%d,%d,%d,%d,%d", &x1, &y1, &x2, &y2, &x3, &y3, &c);
-                display.drawTriangle(x1, y1, x2, y2, x3, y3, c);
-                break;
-
-            case '7':
-                sscanf(s + 3, "%d,%d,%d,%d,%d,%d", &x, &y, &w, &h, &r, &c);
-                display.drawRoundRect(x, y, w, h, r, c);
-                break;
-
-            case '8':
-                sscanf(s + 3, "%d,%d,%d,%d,%d", &x, &y, &w, &h, &c);
-                display.fillRect(x, y, w, h, c);
-                break;
-
-            case '9':
-                sscanf(s + 3, "%d,%d,%d,%d", &x, &y, &r, &c);
-                display.fillCircle(x, y, r, c);
-                break;
-
-            case 'A':
-                sscanf(s + 3, "%d,%d,%d,%d,%d,%d,%d", &x1, &y1, &x2, &y2, &x3, &y3, &c);
-                display.fillTriangle(x1, y1, x2, y2, x3, y3, c);
-                break;
-
-            case 'B':
-                sscanf(s + 3, "%d,%d,%d,%d,%d,%d", &x, &y, &w, &h, &r, &c);
-                display.fillRoundRect(x, y, w, h, r, c);
-                break;
-
-            case 'C':
-                sscanf(s + 3, "\"%2000[^\"]\"", strTemp);
-                n = strlen(strTemp);
-                for (int i = 0; i < n; i++)
-                {
-                    strTemp[i] = toupper(strTemp[i]);
-                }
-                for (int i = 0; i < n; i += 2)
-                {
-                    strTemp[i / 2] = (hexToChar(strTemp[i]) << 4) | (hexToChar(strTemp[i + 1]) & 0x0F);
-                }
-                strTemp[n / 2] = 0;
-                display.print(strTemp);
-                break;
-
-            case 'D':
-                sscanf(s + 3, "%d", &c);
-                display.setTextSize(c);
-                break;
-
-            case 'E':
-                sscanf(s + 3, "%d,%d", &x, &y);
-                display.setCursor(x, y);
-                break;
-
-            case 'F':
-                sscanf(s + 3, "%c", &b);
-                if (b == 'T')
-                    display.setTextWrap(true);
-                if (b == 'F')
-                    display.setTextWrap(false);
-                break;
-
-            case 'G':
-                sscanf(s + 3, "%d", &c);
-                c &= 3;
-                display.setRotation(c);
-                break;
-
-            case 'H':
-                sscanf(s + 3, "%d,%d,\"%149[^\"]\"", &x, &y, strTemp);
-                n = strlen(strTemp);
-                for (int i = 0; i < n; i++)
-                {
-                    strTemp[i] = toupper(strTemp[i]);
-                }
-                for (int i = 0; i < n; i += 2)
-                {
-                    strTemp[i / 2] = (hexToChar(strTemp[i]) << 4) | (hexToChar(strTemp[i + 1]) & 0x0F);
-                }
-                strTemp[n / 2] = 0;
-                r = display.sdCardInit();
-                if (r)
-                {
-                    r = display.drawBitmapFromSd(strTemp, x, y);
-                    Serial.print("#H(");
-                    Serial.print(r, DEC);
-                    Serial.println(")*");
-                    Serial.flush();
-                }
-                else
-                {
-                    Serial.println("#H(-1)*");
-                    Serial.flush();
-                }
-                break;
-
-            case 'K':
-                sscanf(s + 3, "%c", &b);
-                if (b == '1')
-                {
-                    display.clearDisplay();
-                }
-                break;
-
-            case 'L':
-                sscanf(s + 3, "%c", &b);
-                if (b == '1')
-                {
-                    display.display();
-                }
-                break;
-
-            case 'M':
-                sscanf(s + 3, "%d,%d,%d", &y1, &x2, &y2);
-                display.display();
-                break;
-
-            case 'P':
-                sscanf(s + 3, "%c", &b);
-                if (b == '?')
-                {
-                    Serial.print("#P(");
-                    Serial.print(display.readBattery(), 2);
-                    Serial.println(")*");
-                    Serial.flush();
-                }
-                break;
-
-            case 'S':
-                sscanf(s + 3, "%d,%d,\"%149[^\"]\"", &x, &y, strTemp);
-                n = strlen(strTemp);
-                for (int i = 0; i < n; i++)
-                {
-                    strTemp[i] = toupper(strTemp[i]);
-                }
-                for (int i = 0; i < n; i += 2)
-                {
-                    strTemp[i / 2] = (hexToChar(strTemp[i]) << 4) | (hexToChar(strTemp[i + 1]) & 0x0F);
-                }
-                strTemp[n / 2] = 0;
-                r = display.sdCardInit();
-                if (r)
-                {
-                    r = display.drawImage(strTemp, x, y);
-                    Serial.print("#H(");
-                    Serial.print(r, DEC);
-                    Serial.println(")*");
-                    Serial.flush();
-                }
-                else
-                {
-                    Serial.println("#H(-1)*");
-                    Serial.flush();
-                }
-                break;
-            case 'T':
-                int t;
-                sscanf(s + 3, "%d,%d,%d,%d,%d,%d", &x1, &y1, &x2, &y2, &c, &t);
-                display.drawThickLine(x1, y1, x2, y2, c, t);
-                break;
-            case 'U':
-                sscanf(s + 3, "%d,%d,%d,%d,%d", &rx, &ry, &xc, &yc, &c);
-                display.drawElipse(rx, ry, xc, yc, c);
-                break;
-            case 'V':
-                sscanf(s + 3, "%d,%d,%d,%d,%d", &rx, &ry, &xc, &yc, &c);
-                display.fillElipse(rx, ry, xc, yc, c);
-                break;
-            }
-            *s = 0;
-            *e = 0;
-        }
-    }
-}
-
-int hexToChar(char c)
-{
-    if (c >= '0' && c <= '9')
-        return c - '0';
-    if (c >= 'A' && c <= 'F')
-        return c - 'A' + 10;
-    if (c >= 'a' && c <= 'f')
-        return c - 'a' + 10;
-    return -1;
+    // Check if there is incoming data on serial and process commands
+    peripheral->getDataFromSerial(SERIAL_TIMEOUT_MS);
 }
